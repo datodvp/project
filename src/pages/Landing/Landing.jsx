74b67936d25593cbda909d6dc593a545aae18d44
datodvp <gaguadato98@gmail.com>
2022-09-04 01:55:38 +0400

    added landing page responsive

--- src/pages/Landing/Landing.jsx
+++ src/pages/Landing/Landing.jsx
@@ -1,18 +1,15 @@
 import Logo from "../../assets/photos/Logo.svg";
 import DesktopLandingImage from "../../assets/photos/DesktopLandingImage.png";
 import ButtonLink from "../../components/ButtonLink";
+import "./styles.css";
 
 const Landing = () => {
   return (
     <div className="landingPage">
       <img className="landingLogo" src={Logo} alt="Logo" />
       <img className="landingImage" src={DesktopLandingImage} alt="Landing" />
-      <ButtonLink path="/addLaptop" width={387}>
-        ჩანაწერის დამატება
-      </ButtonLink>
-      <ButtonLink path="/Laptops" width={387}>
-        ჩანაწერების სია
-      </ButtonLink>
+      <ButtonLink path="/addLaptop">ჩანაწერის დამატება</ButtonLink>
+      <ButtonLink path="/Laptops">ჩანაწერების სია</ButtonLink>
     </div>
   );
 };
